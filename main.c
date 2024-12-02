ef60c0e6354122976f5cef7652a920651e733a3b
Aryan Mediratta <aryan_02@outlook.com>
2024-12-02 05:54:25 -0600

    Added Sunday check feature

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
@@ -6,5 +7,18 @@ int main(void)
     printf("Enter your name: ");
     scanf("%s", userName);
     printf("Hello %s\n", userName);
+
+    printf("What day is it? ");
+    char day[15];
+    scanf("%s", day);
+
+    if(strcmp(day, "Sunday") == 0)
+    {
+        printf("It's Sunday!\n");
+    }
+    else
+    {
+        printf("It's not Sunday ☹️\n");
+    }
     return 0;
 }
